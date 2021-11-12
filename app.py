2c2373ac371a11a7ddd9375ba9fb3f704fa8ba97
andrerafaelf <andrerafaelff@gmail.com>
2021-11-12 21:00:03 +0000

    Added edit and delete buttons

--- app.py
+++ app.py
@@ -79,12 +79,13 @@ def login():
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    posts = list(mongo.db.posts.find())
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
     if session["user"]:
-        return render_template("profile.html", username=username)
+        return render_template("profile.html", username=username, posts=posts)
 
     return redirect(url_for("login"))
 
